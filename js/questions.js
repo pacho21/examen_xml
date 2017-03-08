b0ee027624819604fe267aa521cad6c2b93d3e85
pacho21 <d3pacho@gmail.com>
2017-03-08 11:03:24 +0100

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -42,12 +42,13 @@ window.onload = function(){
 function gestionarXml(contXml){
   var xmlDoc = contXml.responseXML;
   var inpt = document.createElement("input");
+  var nopciones;
   /*radio1*/
 /*Pregunta tipo 'radio' nº 1.*/
   document.getElementById('q01').innerHTML=xmlDoc.getElementsByTagName("title")[0].innerHTML;
-  res_rad_1 = xmlDoc.getElementById("q01").getElementsByTagName("answer")[0].innerHTML;/*Guardamos respuesta/s correctas para comprobación posterior.*/
+  answRadio1 = xmlDoc.getElementById("q_01").getElementsByTagName("answer")[0].innerHTML;/*Guardamos respuesta/s correctas para comprobación posterior.*/
   select=document.getElementById("in_1");
-  nopciones = xmlDoc.getElementById("q01").getElementsByTagName("option").length;
+  nopciones = xmlDoc.getElementById("q_01").getElementsByTagName("option").length;
   for (i = 0; i < nopciones; i++)
   { 
     inpt = document.createElement("input");
@@ -61,9 +62,9 @@ function gestionarXml(contXml){
   
   /*radio2*/
    document.getElementById('q02').innerHTML=xmlDoc.getElementsByTagName("title")[1].innerHTML;
-  res_rad_1 = xmlDoc.getElementById("q02").getElementsByTagName("answer")[0].innerHTML;/*Guardamos respuesta/s correctas para comprobación posterior.*/
+  answRadio2 = xmlDoc.getElementById("q_02").getElementsByTagName("answer")[0].innerHTML;/*Guardamos respuesta/s correctas para comprobación posterior.*/
   select=document.getElementById("in_2");
-  nopciones = xmlDoc.getElementById("q02").getElementsByTagName("option").length;
+  nopciones = xmlDoc.getElementById("q_02").getElementsByTagName("option").length;
   for (i = 0; i < nopciones; i++)
   { 
     inpt = document.createElement("input");
